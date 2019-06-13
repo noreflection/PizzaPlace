c2e0edf123a7d5844ae3e6f229064718cb189f35
noreflection <a191a191a@gmail.com>
2019-06-13 19:45:18 +0300

    before components lib

--- PizzaStore.Client/Pages/Timer.cs
+++ PizzaStore.Client/Pages/Timer.cs
@@ -3,11 +3,30 @@ using Microsoft.AspNetCore.Components;
 
 namespace PizzaStore.Client.Pages
 {
-    public class DismissibleAlertViewModel : ComponentBase
+    public class Timer : ComponentBase
     {
+        [Parameter]
+        protected double TimeInSeconds { get; set; }
+        
+        [Parameter]
+        protected Action Tick { get; set; }
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+            
+            var timer = new System.Threading.Timer(
+                  callback: _=> Tick.Invoke(),
+                  state: null,
+                  dueTime: TimeSpan.FromSeconds(TimeInSeconds),
+                  period: System.Threading.Timeout.InfiniteTimeSpan);
+            
+        }
+        
         private bool show = true;
         
         [Parameter]
+        
         protected bool Show
         {
             get => show;
@@ -20,11 +39,11 @@ namespace PizzaStore.Client.Pages
                 }
             }
         }
-        
         [Parameter]
         protected Action<bool> ShowChanged { get; set; }
         [Parameter]
         protected RenderFragment ChildContent { get; set; }
+        
         public void Dismiss()
         {
             Show = false;
